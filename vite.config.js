b74a565277c15beac5ecc20a0415cbc78454fb77
avi.04xx <avisvm12@gmail.com>
2026-04-21 15:28:24 +0530

    fix base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AI-CRAFTER/',
+  base: "/",   // ✅ change this
   plugins: [react()],
 })
